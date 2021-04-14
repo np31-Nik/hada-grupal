6af705b0fc2d9e614c50c0ed08c58b70f81e7e71
Juan Carlos Sanchez Gonzalez <superhijobatman@hotmail.com>
2021-04-14 16:41:21 +0200

    propiedades privadas

--- library/EN/ENBanco.cs
+++ library/EN/ENBanco.cs
@@ -8,5 +8,27 @@ namespace library.EN
 {
     class ENBanco
     {
+        private string banco;
+
+        private int telefono;
+
+        private string email;
+
+        public string BANCO
+        {
+            get { return banco; }
+            set { banco = value; }
+        }
+        public int TELEFONO
+        {
+            get { return telefono; }
+            set { telefono = value; }
+        }
+
+        public string EMAIL 
+        {
+            get { return email; }
+            set { email = value; }
+        }
     }
 }
--- library/EN/ENHipoteca.cs
+++ library/EN/ENHipoteca.cs
@@ -8,5 +8,52 @@ namespace library.EN
 {
     class ENHipoteca
     {
+        private int id;
+
+        private string Fechadesde;
+
+        private string Fechahasta;
+
+        private float intereses;
+
+        private int numSolicitudes;
+
+        private string banco;
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string FECHADESDE
+        {
+            get { return Fechadesde; }
+            set { Fechadesde = value; }
+        }
+
+        public string FECHAHASTA
+        {
+            get { return Fechahasta; }
+            set { Fechahasta = value; }
+        }
+
+        public float INTERESES
+        {
+            get { return intereses; }
+            set { intereses = value; }
+        }
+
+        public int NUMSOLICITUDES
+        {
+            get { return numSolicitudes; }
+            set { numSolicitudes = value; }
+        }
+
+        public string BANCO
+        {
+            get { return banco; }
+            set { banco = value; }
+        }
     }
 }
