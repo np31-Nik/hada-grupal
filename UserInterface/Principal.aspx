d122d4d81b7ab0905ca99692c594d7746734123d
Juan Carlos Sanchez Gonzalez <superhijobatman@hotmail.com>
2021-04-30 01:29:54 +0200

    barra buscadora prototipo

--- UserInterface/Principal.aspx
+++ UserInterface/Principal.aspx
@@ -1,6 +1,26 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Principal.aspx.cs" Inherits="UserInterface.Principal" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server" >
+    <div style="background-color: #151719; width: 100%; color:white; padding:10px;">
+        ID: <asp:TextBox id="clave" TextMode="SingleLine" Columns="10" runat="server" />
+        
+        Nombre: <asp:TextBox id="nombre" TextMode="SingleLine" Columns="20" runat="server" />
+        
+        Precio Mínimo: <asp:TextBox ID="BoxNumeros" runat="server"/>
+           
+        Precio Límite: <asp:TextBox ID="BoxNumeros2" runat="server"/>
+          
+        <asp:DropDownList runat="server" ID="ALQoVENT" AutoPostBack="True">
+            <asp:ListItem Selected="True" Value="Alquiler"> Alquiler </asp:ListItem>
+            <asp:ListItem Value="Venta"> Venta </asp:ListItem>
+        </asp:DropDownList>
+        <asp:DropDownList runat="server" ID="CasaoCoche" AutoPostBack="True">
+            <asp:ListItem Selected="True" Value="Propiedad"> Propiedad </asp:ListItem>
+            <asp:ListItem Value="Coche"> Coche </asp:ListItem>
+        </asp:DropDownList>
+        CP: <asp:TextBox id="cp" TextMode="SingleLine" Columns="5" runat="server" />
+        
+        Localidad: <asp:TextBox id="local" TextMode="SingleLine" Columns="20" runat="server" />
+    </div>
 </asp:Content>
--- UserInterface/Principal.aspx.designer.cs
+++ UserInterface/Principal.aspx.designer.cs
@@ -13,5 +13,77 @@ namespace UserInterface
 
     public partial class Principal
     {
+
+        /// <summary>
+        /// Control clave.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox clave;
+
+        /// <summary>
+        /// Control nombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox nombre;
+
+        /// <summary>
+        /// Control BoxNumeros.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox BoxNumeros;
+
+        /// <summary>
+        /// Control BoxNumeros2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox BoxNumeros2;
+
+        /// <summary>
+        /// Control ALQoVENT.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ALQoVENT;
+
+        /// <summary>
+        /// Control CasaoCoche.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList CasaoCoche;
+
+        /// <summary>
+        /// Control cp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox cp;
+
+        /// <summary>
+        /// Control local.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox local;
     }
 }
