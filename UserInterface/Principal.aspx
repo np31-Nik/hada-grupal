69c439c00cc843ad209d43177c8d5e5eec3db49f
Juan Carlos Sanchez Gonzalez <superhijobatman@hotmail.com>
2021-04-30 20:51:29 +0200

    prototipo principal

--- UserInterface/Principal.aspx
+++ UserInterface/Principal.aspx
@@ -2,31 +2,90 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server" >
-    <div style="background-color: #151719; width: 100%; color:white; padding:10px; text-align:center ">
-          
-        <asp:DropDownList runat="server" ID="CasaoCoche" AutoPostBack="True" Height="25px" Width="200px">
+    <div style="background-color: #151719; color:white; padding:10px; text-align:center ">
+        Referencia: <asp:TextBox ID="ref" runat="server" Height="25px" Width="200px" />
+        
+        <asp:DropDownList runat="server" ID="CasaoCoche" AutoPostBack="True" Height="25px" Width="175px">
             <asp:ListItem Selected="True" Value="Propiedad"> Propiedad </asp:ListItem>
             <asp:ListItem Value="Coche"> Coche </asp:ListItem>
         </asp:DropDownList>
 
-        <asp:DropDownList runat="server" ID="ALQoVENT" AutoPostBack="True" Height="25px" Width="200px">
+        <asp:DropDownList runat="server" ID="ALQoVENT" AutoPostBack="True" Height="25px" Width="175px">
             <asp:ListItem Selected="True" Value="Alquiler"> Alquiler </asp:ListItem>
             <asp:ListItem Value="Venta"> Venta </asp:ListItem>
         </asp:DropDownList>
         
-        <asp:DropDownList runat="server" ID="localidades" AutoPostBack="True" Height="25px" Width="200px">
+        <asp:DropDownList runat="server" ID="localidades" AutoPostBack="True" Height="25px" Width="175px">
             <asp:ListItem Selected="True" Value="Localidad"> Localidad </asp:ListItem>
             <asp:ListItem Value="Alicante"> Alicante </asp:ListItem>
         </asp:DropDownList>
 
-        Precio Mínimo: <asp:TextBox ID="BoxNumeros" runat="server" Height="25px" Width="200px" />
+        Precio Mínimo: <asp:TextBox ID="BoxNumeros" runat="server" Height="25px" Width="175px" />
            
-        Precio Límite: <asp:TextBox ID="BoxNumeros2" runat="server" Height="25px" Width="200px"/>
+        Precio Límite: <asp:TextBox ID="BoxNumeros2" runat="server" Height="25px" Width="175px"/>
 
         <asp:ImageButton ID="LupaBuscar" runat="server" ImageUrl="~/imagenes/455-4559731_icono-de-lupa-para-legal-design-thinking-lupa.png"  Width="30px" ImageAlign="Middle" Height="30px"/>
         
     </div>
-    <div>
-        <br /><br /><br /><br /><br /><br /><br />
+    <div style="font-size:15px; text-align:center; font-family:'sans-serif';">
+        <br /><br /><br /><br /><br />
+
+        <p style="font-size:40px; text-align:center; font-family:'sans-serif'; background-color:#151719; color:white;">
+            <b>
+                Portada
+            </b>
+        </p>
+
+        <br /><br /><br /><br /><br />
+
+        <asp:Image ID="Image1" runat="server" ImageUrl="~/imagenes/agencias-inmobiliarias-idolatria-de-la-exclusiva.jpg" Width="720px" Height="450px" ImageAlign="Left"/>
+
+        <asp:Image ID="Image2" runat="server" ImageUrl="~/imagenes/productividad-agencia-inmobiliaria-2-e1571754106299.jpg"  Width="720px" Height="450px" ImageAlign="Right"/>
+
+        <br /><br /><br /><br /><br /><br />
+        
+        <p style="font-size:15px; text-align:left">
+            <br /><br /><br /><br /><br /><br />
+            info..............................................................................................................................................
+        </p>
+
+        <br /><br /><br /><br /><br /><br />
+
+        <asp:Image ID="Image3" runat="server" ImageUrl="~/imagenes/shutterstock_499614244-1080x675.jpg" Width="720px" Height="450px" ImageAlign="AbsMiddle"/>
+        
+        <br /><br /><br /><br /><br />
+
+        <p style="color:white; text-align:center; font-family:'sans-serif'; font-size:30px; background-color:#151719;">
+            <b>
+                Ofertas de interés
+            </b>
+        </p>
+            <br /><br /><br /><br /><br />
+
+            <asp:ImageButton ID="ImageButton1" runat="server" Width="360px" Height="240px" BorderWidth="20px"/>
+            <asp:ImageButton ID="ImageButton6" runat="server" Width="360px" Height="240px" BorderWidth="20px"/>
+
+            <br /><br /><br /><br /><br />
+
+            <asp:ImageButton ID="ImageButton2" runat="server" Width="360px" Height="240px" BorderWidth="20px"/>
+            <asp:ImageButton ID="ImageButton7" runat="server" Width="360px" Height="240px" BorderWidth="20px"/>
+            
+            <br /><br /><br /><br /><br />
+
+            <asp:ImageButton ID="ImageButton3" runat="server" Width="360px" Height="240px" BorderWidth="20px"/>
+            <asp:ImageButton ID="ImageButton8" runat="server" Width="360px" Height="240px" BorderWidth="20px"/>
+            
+            <br /><br /><br /><br /><br />
+
+            <asp:ImageButton ID="ImageButton4" runat="server" Width="360px" Height="240px" BorderWidth="20px"/>
+            <asp:ImageButton ID="ImageButton9" runat="server" Width="360px" Height="240px" BorderWidth="20px"/>
+
+            <br /><br /><br /><br /><br />
+
+            <asp:ImageButton ID="ImageButton5" runat="server" Width="360px" Height="240px" BorderWidth="20px"/>
+            <asp:ImageButton ID="ImageButton10" runat="server" Width="360px" Height="240px" BorderWidth="20px"/>
+            
+            <br /><br /><br /><br /><br />
+
     </div>
 </asp:Content>
--- UserInterface/Principal.aspx.designer.cs
+++ UserInterface/Principal.aspx.designer.cs
@@ -15,6 +15,15 @@ namespace UserInterface
     {
 
         /// <summary>
+        /// Control ref.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox @ref;
+
+        /// <summary>
         /// Control CasaoCoche.
         /// </summary>
         /// <remarks>
@@ -67,5 +76,122 @@ namespace UserInterface
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ImageButton LupaBuscar;
+
+        /// <summary>
+        /// Control Image1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image1;
+
+        /// <summary>
+        /// Control Image2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image2;
+
+        /// <summary>
+        /// Control Image3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image3;
+
+        /// <summary>
+        /// Control ImageButton1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton1;
+
+        /// <summary>
+        /// Control ImageButton6.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton6;
+
+        /// <summary>
+        /// Control ImageButton2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton2;
+
+        /// <summary>
+        /// Control ImageButton7.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton7;
+
+        /// <summary>
+        /// Control ImageButton3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton3;
+
+        /// <summary>
+        /// Control ImageButton8.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton8;
+
+        /// <summary>
+        /// Control ImageButton4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton4;
+
+        /// <summary>
+        /// Control ImageButton9.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton9;
+
+        /// <summary>
+        /// Control ImageButton5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton5;
+
+        /// <summary>
+        /// Control ImageButton10.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton10;
     }
 }
