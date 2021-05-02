5a765f4c7884d1858c4dd310ca5485f8d29e2921
juan <jrmg14@alu.ua.es>
2021-05-02 11:19:30 +0200

    implementacion interfaz registro

---
+++ UserInterface/Registro.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Registro.aspx.cs" Inherits="UserInterface.Registro" %>
+
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+
+<body>
+    background-color: #f1f1f1;
+    <fid="formorm 1" runat="server">
+        <h2>Registro de usuarios:</h2>
+        <div runat="server" id="main">
+            Mayor de edad:
+             <asp:RadioButtonList 
+                ID="RadioButtonList1"
+                runat="server"
+                AutoPostBack="true"
+                OnSelectedIndexChanged="RadioButtonList1_SelectedIndexChanged">
+                <asp:ListItem>Si</asp:ListItem>
+                <asp:ListItem>No</asp:ListItem>
+            </asp:RadioButtonList>
+            Ingrese su nombre:
+            <asp:TextBox ID=" nombre" runat="server">  </asp:TextBox> <br /><br />
+            Ingrese sus apellidos:
+            <asp:TextBox ID=" apellidos" runat="server"> </asp:TextBox> <br /><br />
+            Ingrese su correo:
+            <asp:TextBox ID="email" runat="server"> </asp:TextBox> <br /><br />
+            Ingrese su contraseña:
+            <asp:TextBox ID="password" runat="server"> </asp:TextBox> <br /><br />
+            Vuelva a introducirla:
+            <asp:TextBox ID="password2" runat="server"> </asp:TextBox> <br /><br />
+            Introduzca su numero de telefono:
+            <asp:TextBox ID=" movil" runat="server"> </asp:TextBox> <br /><br />
+            Introzuca su numero de telefono fijo:
+             <asp:TextBox ID="fijo" runat="server"> </asp:TextBox> <br /><br />
+            </p> Estas de acuerdo con <a href ="#">Términos y condiciones</a></p>
+                <asp:RadioButtonList 
+                     ID="RadioButtonList2"
+                     runat="server"
+                     AutoPostBack="true"
+                      OnSelectedIndexChanged="RadioButtonList1_SelectedIndexChanged">
+                     <asp:ListItem>Si</asp:ListItem>
+                    <asp:ListItem>No</asp:ListItem>
+                </asp:RadioButtonList>
+            </div>
+             <asp:Button ID="registar" runat="server" Text="Registrar" onClick="RegistarClick"/>
+            <p> <a href ="#">¿Ya tiene cuenta?</a></p>
+    </form>
+</body>
+        
+
+
+
+
+            
+
+
+
+       
+
---
+++ UserInterface/Registro.aspx.designer.cs
@@ -0,0 +1,116 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace UserInterface
+{
+
+
+    public partial class Registro
+    {
+
+        /// <summary>
+        /// Control main.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl main;
+
+        /// <summary>
+        /// Control RadioButtonList1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList RadioButtonList1;
+
+        /// <summary>
+        /// Control  nombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox nombre;
+
+        /// <summary>
+        /// Control  apellidos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox apellidos;
+
+        /// <summary>
+        /// Control email.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox email;
+
+        /// <summary>
+        /// Control password.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox password;
+
+        /// <summary>
+        /// Control password2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox password2;
+
+        /// <summary>
+        /// Control  movil.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox movil;
+
+        /// <summary>
+        /// Control fijo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox fijo;
+
+        /// <summary>
+        /// Control RadioButtonList2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList RadioButtonList2;
+
+        /// <summary>
+        /// Control registar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button registar;
+    }
+}
