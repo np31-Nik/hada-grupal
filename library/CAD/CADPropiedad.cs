d0aed945bd09c987c215c327077e2482c27d9b21
Serhii Vidernikov <sv54@gcloud.ua.es>
2021-05-14 16:40:21 +0200

    Create corregido, read añadido

--- library/CAD/CADPropiedad.cs
+++ library/CAD/CADPropiedad.cs
@@ -16,9 +16,9 @@ namespace library
         {
             bool creado = false;
             //Falta anyadir mas atibutos a la BD
-            string comando = "Insert INTO [dbo].[Propiedad] (id,numCatastral, superficie, dorm, bano,tipo)" +
+            string comando = "Insert INTO [dbo].[Propiedad] (id,numCatastral, superficie, dorm, bano, tipo)" +
                 "VALUES ('" + id + "', '" + en.numCatastral + "', '" + en.superficie + "', "
-                + en.habitaciones + "', " + en.banyos + ")";
+                + en.habitaciones + "', " + en.banyos + "', " + en.tipo + ")";
 
             try
             {
@@ -45,9 +45,47 @@ namespace library
         public bool readPropiedad(ENPropiedad en, uint id)
         {
             bool read = false;
+            string comando = "select * From [dbo].[Propiedad] where anuncio='" + id + "'";
 
+            try
+            {
+                SqlConnection conn = null;
+                conn = new SqlConnection(constring);
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(comando, conn);
+                SqlDataReader buscar = cmd.ExecuteReader();
+                while (buscar.Read() && !read)
+                {
+                    if (buscar["id"].ToString() == id.ToString())
+                    {
+                        
+                        id = (uint)int.Parse(buscar["id"].ToString());
+                        en.numCatastral = buscar["numCatastral"].ToString();
+                        en.superficie = int.Parse(buscar["superficie"].ToString());
+                        en.tipo = (ENTipoPropiedad)buscar["tipo"];
+                        en.banyos = int.Parse(buscar["bano"].ToString());
+                        en.habitaciones = int.Parse(buscar["dorm"].ToString());
+                    }
+                }
+                buscar.Close();
+                conn.Close();
+                read = true;
+
+            }
+            catch (SqlException ex)
+            {
+                read = false;
+                Console.WriteLine("User operation hasfailed.Error: {0}", ex.Message);
+            }
+            catch (Exception ex)
+            {
+                read = false;
+                Console.WriteLine("User operation hasfailed.Error: {0}", ex.Message);
+
+            }
             return read;
         }
+    
         public bool updatePropiedad(ENPropiedad en, uint id)
         {
             bool update = false;
