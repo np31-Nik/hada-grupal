9e6dc71c54bd2067235928195c874f1993f9f6cb
np31-ua <np31@alu.ua.es>
2021-05-19 23:17:04 +0200

    Carga de datos en Page_Load (Anuncio.aspx)

--- UserInterface/Anuncio.aspx.cs
+++ UserInterface/Anuncio.aspx.cs
@@ -17,7 +17,39 @@ namespace UserInterface
                 ENAnuncio en = new ENAnuncio();
                 en.id= uint.Parse(Request.QueryString["anuncio_id"]);
                 en.readAnuncio();
-                
+                /*
+                TITULO.Text = en.titulo;
+                PRECIO.Text = en.precio.ToString();
+                ENUsuario propietario = en.usuario;
+                propietario.readUsuario();
+                PROPIETARIO.Text = propietario.Nombre + " " + propietario.Apellido;
+                TELEFONO.Text = propietario.Telefono;
+                EMAIL.Text = propietario.Email;
+                LOCALIDAD.Text = en.localidad;
+                COD_POSTAL.Text = en.cp.ToString();
+
+                switch (en.tipo)
+                {
+                    case "vehiculo":
+                        ENCoche coche = new ENCoche();
+                        coche.id = en.id;
+                        coche.readCoche();
+                        ANYO.Text = coche.anyo.ToString();
+                        MARCA.Text = coche.marca.tipo;
+                        TIPO_COCHE.Text = coche.tipo.categoria;
+                        break;
+                    case "propiedad":
+                        ENPropiedad prop = new ENPropiedad();
+                        prop.id = en.id;
+                        prop.readPropiedad();
+                        REF_CAT.Text = prop.numCatastral;
+                        SUPERFICIE.Text = prop.superficie.ToString();
+                        DORMITORIOS.Text = prop.habitaciones.ToString();
+                        BANYOS.Text = prop.banyos.ToString();
+                        TIPO_PROPIEDAD.Text = prop.tipo;
+                        break;
+                }
+                */
             }
         }
         protected void CalcularHipoteca(object sender,EventArgs e)
