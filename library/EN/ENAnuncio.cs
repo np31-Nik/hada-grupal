3c25e1980eebd2821da6eb701ffcc227888328d1
np31-ua <np31@alu.ua.es>
2021-05-25 17:56:33 +0200

    cambio en ENAnuncio

--- library/EN/ENAnuncio.cs
+++ library/EN/ENAnuncio.cs
@@ -177,13 +177,13 @@ namespace library
             bool read = false;
             if (anuncio.readAnuncio(this))
             {
-                if (categoria == "Coche")
+                if (categoria == "coche")
                 {
                     coche.id = this.id;
                     if (coche.readCoche(coche))
                         read = true;
                 }
-                else if (categoria == "Propiedad") {
+                else if (categoria == "propiedad") {
                     prop.id = this.id;
                     if (prop.readPropiedad(prop))
                         read = true;
